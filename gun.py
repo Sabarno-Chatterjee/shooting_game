8256a7598c487ba0d7ce5397209b4aa5bad14f02
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-22 12:43:33 +0530

    generates a bullet, need to work on the functionality

--- gun.py
+++ gun.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
 
+
 class Gun(Turtle):
     def __init__(self):
         super().__init__()
@@ -7,22 +8,26 @@ class Gun(Turtle):
         self.setheading(90)
         self.shapesize(stretch_wid=3, stretch_len=1.5)
         self.penup()
-        self.goto((0,-280))
+        self.goto((0, -280))
         self.move_left()
         self.move_right()
 
-
     def move_right(self):
         x_position = self.xcor() + 20
-        self.goto(x_position, y = self.ycor() )
+        self.goto(x_position, y=self.ycor())
 
     def move_left(self):
         x_position = self.xcor() - 20
         self.goto(x_position, y=self.ycor())
 
-class bullet(Turtle):
-    def
-        self.shape("circle")
-        self.penup()
-        self.shapesize(stretch_wid=1, stretch_len=1)
 
+class Bullet(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.penup()
+        self.shapesize(stretch_wid=0.5, stretch_len=0.2)
+
+    def shoot(self):
+        if self.xcor() < 300:
+            self.forward(20)
--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from turtle import Screen
-from gun import Gun
+from gun import Gun, Bullet
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.listen()
+screen.tracer(0)
 
 
 
 gun = Gun()
-
+bullet = Bullet()
 screen.onkey(fun=gun.move_right, key="Right")
 screen.onkey(fun=gun.move_left, key="Left")
+screen.onkey(fun=bullet.shoot, key="space")
 
 
+is_game_on = True
 
-
+while is_game_on:
+    screen.update()
+    time.sleep(1)
 
 
 
